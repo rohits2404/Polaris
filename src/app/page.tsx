73ae1fce4a578330c6772431292b2b5e09656caf
Rohit Sharma <rohits8731@gmail.com>
2026-01-07 19:59:31 +0530

    01: Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+    return (
+        <Button>
+            Click Me
+        </Button>
+    )
+}
+
+export default Home
