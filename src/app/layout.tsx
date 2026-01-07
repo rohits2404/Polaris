2e9a1a5a63d2c2170ce219997d5426daa806dabe
Rohit Sharma <rohits8731@gmail.com>
2026-01-07 20:27:26 +0530

    02: Authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({
     variable: "--font-inter",
@@ -25,19 +27,23 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
-            <body
-            className={`${inter.variable} ${plexMono.variable} antialiased`}
-            >
-                <ThemeProvider
-                attribute="class"
-                defaultTheme="light"
-                enableSystem
-                disableTransitionOnChange
+        <ClerkProvider
+        appearance={dark}
+        >
+            <html lang="en" suppressHydrationWarning>
+                <body
+                className={`${inter.variable} ${plexMono.variable} antialiased`}
                 >
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
+                    <ThemeProvider
+                    attribute="class"
+                    defaultTheme="light"
+                    enableSystem
+                    disableTransitionOnChange
+                    >
+                        {children}
+                    </ThemeProvider>
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
