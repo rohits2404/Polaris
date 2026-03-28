977cba9846dbd6dbd8519bc3e3d7bca82abd7980
Rohit Sharma <rohits8731@gmail.com>
2026-03-29 00:50:46 +0530

    03: Database Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,8 @@ import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import { dark } from "@clerk/ui/themes";
+import { Providers } from "@/components/provider";
 
 const inter = Inter({
     subsets:['latin'],
@@ -35,32 +33,10 @@ export default function RootLayout({
         className={cn("h-full", "antialiased", inter.variable, plexMono.variable, "font-sans")}
         >
             <body className="min-h-full flex flex-col">
-                <ClerkProvider appearance={{
-                    theme: dark
-                }}>
-                    <header>
-                        <Show when="signed-out">
-                            <SignInButton />
-                            <SignUpButton>
-                                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                                    Sign Up
-                                </button>
-                            </SignUpButton>
-                        </Show>
-                        <Show when="signed-in">
-                            <UserButton />
-                        </Show>
-                    </header>
-                    <ThemeProvider
-                    attribute="class"
-                    defaultTheme="dark"
-                    enableSystem
-                    disableTransitionOnChange
-                    >
-                        {children}
-                        <Toaster/>
-                    </ThemeProvider>
-                </ClerkProvider>
+                <Providers>
+                    {children}
+                    <Toaster/>
+                </Providers>
             </body>
         </html>
     );
