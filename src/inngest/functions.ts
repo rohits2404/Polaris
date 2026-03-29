bb719b93b44fe92995228ea39b0e19417614d303
Rohit Sharma <rohits8731@gmail.com>
2026-03-29 12:41:21 +0530

    05: Firecrawal AI

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,6 +1,9 @@
 import { generateText } from "ai";
 import { inngest } from "./client";
 import { createOpenAI } from "@ai-sdk/openai";
+import { firecrawl } from "@/lib/firecrawl";
+
+const URL_REGEX = /https?:\/\/[^\s]+/g;
 
 const groq = createOpenAI({
     apiKey: process.env.GROQ_API_KEY,
@@ -12,11 +15,35 @@ export const demoGenerate = inngest.createFunction(
         id: "demo-generate",
         triggers: [{ event: "demo/generate" }],
     },
-    async ({ step }) => {
+    async ({ event, step }) => {
+        const { prompt } = event.data as { prompt: string };
+
+        if (!prompt) {
+            throw new Error(`Missing prompt in event.data. Received: ${JSON.stringify(event.data)}`);
+        }
+
+        const urls = await step.run("exctract-urls", async () => {
+            return prompt.match(URL_REGEX) ?? [];
+        }) as string[];
+
+        const scrapedContent = await step.run("scrape-urls", async () => {
+            const results = await Promise.all(
+                urls.map(async (url) => {
+                    const result = await firecrawl.scrape(
+                        url,
+                        { formats: ["markdown"] },
+                    );
+                    return result.markdown ?? null;
+                })
+            );
+            return results.filter(Boolean).join("\n\n");
+        });
+
+        const finalPrompt = scrapedContent ? `Context:\n${scrapedContent}\n\nQuestion: ${prompt}` : prompt;
         await step.run("generate-text", async () => {
             return await generateText({
                 model: groq("openai/gpt-oss-20b"),
-                prompt: "Write a vegetarian lasagna recipe for 4 people.",
+                prompt: finalPrompt
             });
         });
     }
